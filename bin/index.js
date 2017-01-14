ba968db878a9aeeab48c86f40d6b60ee55ea7290
davidalves1 <alves.david@outlook.com>
2017-01-14 20:10:13 -0200

    Initial commit

---
+++ bin/index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+'use strict';
+
+const meow = require('meow');
+
+const cli = meow(`
+    Modo de uso:
+      $ gasto-ar <potencia-do-ar-condicionado> <horas-de-uso-diario>
+
+    Exemplo:
+      $ gasto-ar 680 8
+
+`, {});
+
+if (cli.input.lenght > 2) {
+	console.log('Muitos parâmetros encontrados. Informe apenas a potência e as horas de uso do ar-condicionado.');
+	return;
+}
+
+let potencia = cli.input[0];
+let horas = cli.input[1];
+
+if (typeof potencia !== 'number' || typeof horas !== 'number') {
+	console.log('Informe a potência do seu ar-codicionado e as horas de uso corretamente');
+	return;
+}
+
+if (horas === 0 || horas > 24) {
+	console.log('Informe as horas de uso corretamente');
+	return;
+}
+
+// Converte para quilowatt
+potencia = potencia / 1000;
+
+// Fórmula: potência * horas de uso * capacidade de rendimento médio * dias do mês * preço médio do quilowatt
+let resultado = potencia * horas * 0.7 * 30 * 0.45;
+
+console.log(`O gasto médio do seu ar-codicionado é de aproximadamente R$ ${resultado.toFixed(2)} mensais.`);
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Clima-App
+
+Descubra qual a média de gasto mensal do seu ar condicionado
+
+## Instalação
+
+```shell
+$ npm install -g gasto-ar
+```
+
+## Modo de usar
+
+```shell
+$ gasto-ar <potencia-ar-condicionado> <horas-de-uso-diario>
+$ gasto-ar 680 8
+```
+
+### Contribuições
+
+Crie uma [issue](https://github.com/davidalves1/gasto-ar/issues/new) ou envie um **pull request** e nos ajude a melhorar o projeto, toda contribuição é muito bem vinda!
+
+### Licença
+
+ISC © David Alves de Souza
